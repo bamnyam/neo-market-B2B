729a15cbf7f60b48c0fff4bd39d46586cba307dc
Firsikita <nikitafirsov191@gmail.com>
2026-05-15 18:03:05 +0500

    update test_settings

--- B2B/config/test_settings.py
+++ B2B/config/test_settings.py
@@ -1,16 +1,25 @@
+import atexit
 import os
 
 os.environ["TESTCONTAINERS_RYUK_DISABLED"] = "true"
 
-from testcontainers.postgres import PostgresContainer
+from testcontainers.postgres import PostgresContainer  # noqa: E402
 
-postgres = PostgresContainer("postgres:16")
-postgres.start()
 
-os.environ["POSTGRES_DB"] = postgres.dbname
-os.environ["POSTGRES_USER"] = postgres.username
-os.environ["POSTGRES_PASSWORD"] = postgres.password
-os.environ["POSTGRES_HOST"] = postgres.get_container_host_ip()
-os.environ["POSTGRES_PORT"] = str(postgres.get_exposed_port(5432))
+_postgres = PostgresContainer("postgres:16")
+_postgres.start()
+
+
+def _stop_postgres_container():
+    _postgres.stop()
+
+
+atexit.register(_stop_postgres_container)
+
+os.environ["POSTGRES_DB"] = _postgres.dbname
+os.environ["POSTGRES_USER"] = _postgres.username
+os.environ["POSTGRES_PASSWORD"] = _postgres.password
+os.environ["POSTGRES_HOST"] = _postgres.get_container_host_ip()
+os.environ["POSTGRES_PORT"] = str(_postgres.get_exposed_port(5432))
 
 from config.settings import *  # noqa: E402,F403
